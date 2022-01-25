b36f16676f77eb9cecc99c1e197434a164b52580
xuyangyang2019 <15268296911@163.com>
2022-01-25 18:02:59 +0800

    任务列表ui

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -140,3 +140,16 @@ Page({
     })
   }
 })
+
+// Component({ 
+//   pageLifetimes: { 
+//     show() { 
+//       if (typeof this.getTabBar === 'function' && 
+//         this.getTabBar()) { 
+//         this.getTabBar().setData({ 
+//           selected: 0 
+//         }) 
+//       } 
+//     } 
+//   } 
+// }) 
--- miniprogram/pages/taskList/taskList.js
+++ miniprogram/pages/taskList/taskList.js
@@ -1,14 +1,22 @@
 const app = getApp()
+const order = ['demo1', 'demo2', 'demo3']
 
 // pages/index3/index3.js
 Page({
 
+  onShareAppMessage() {
+    return {
+      title: 'scroll-view',
+      path: 'page/component/pages/scroll-view/scroll-view'
+    }
+  },
   /**
    * 页面的初始数据
    */
   data: {
-    step: 1,
     openid: '',
+    taskList: [],
+    toView: 'green'
   },
 
   /**
@@ -26,60 +34,196 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log('onShow')
+    this.onQueryTask()
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
+    console.log('onHide')
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
     console.log('用户点击右上角分享')
+  },
+
+  onAdd: function () {
+    const db = wx.cloud.database()
+    db.collection('counters').add({
+      data: {
+        count: 1
+      },
+      success: res => {
+        // 在返回结果中会包含新创建的记录的 _id
+        this.setData({
+          counterId: res._id,
+          count: 1
+        })
+        wx.showToast({
+          title: '新增记录成功',
+        })
+        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '新增记录失败'
+        })
+        console.error('[数据库] [新增记录] 失败：', err)
+      }
+    })
+  },
+
+  onQueryTask: function () {
+    console.log(this.data.openid)
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('task').where({
+      _openid: this.data.openid
+    }).get({
+      success: res => {
+        console.log('[数据库] [查询记录] 成功: ', res)
+        this.setData({
+          taskList: res.data
+          // taskList: JSON.stringify(res.data, null, 2)
+        })
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+
+  onCounterInc: function () {
+    const db = wx.cloud.database()
+    const newCount = this.data.count + 1
+    db.collection('counters').doc(this.data.counterId).update({
+      data: {
+        count: newCount
+      },
+      success: res => {
+        this.setData({
+          count: newCount
+        })
+      },
+      fail: err => {
+        icon: 'none',
+        console.error('[数据库] [更新记录] 失败：', err)
+      }
+    })
+  },
+
+  onCounterDec: function () {
+    const db = wx.cloud.database()
+    const newCount = this.data.count - 1
+    db.collection('counters').doc(this.data.counterId).update({
+      data: {
+        count: newCount
+      },
+      success: res => {
+        this.setData({
+          count: newCount
+        })
+      },
+      fail: err => {
+        icon: 'none',
+        console.error('[数据库] [更新记录] 失败：', err)
+      }
+    })
+  },
+
+  onRemove: function () {
+    if (this.data.counterId) {
+      const db = wx.cloud.database()
+      db.collection('counters').doc(this.data.counterId).remove({
+        success: res => {
+          wx.showToast({
+            title: '删除成功',
+          })
+          this.setData({
+            counterId: '',
+            count: null,
+          })
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '删除失败',
+          })
+          console.error('[数据库] [删除记录] 失败：', err)
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '无记录可删，请见创建一个记录',
+      })
+    }
+  },
+  upper(e) {
+    console.log(e)
+  },
+
+  lower(e) {
+    console.log(e)
+  },
+
+  scroll(e) {
+    console.log(e)
+  },
+
+  scrollToTop() {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+
+  tap() {
+    for (let i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1],
+          scrollTop: (i + 1) * 200
+        })
+        break
+      }
+    }
+  },
+
+  tapMove() {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
   }
 })
-
-// Component({ 
-//   pageLifetimes: { 
-//     show() { 
-//       if (typeof this.getTabBar === 'function' && 
-//         this.getTabBar()) { 
-//         this.getTabBar().setData({ 
-//           selected: 0 
-//         }) 
-//       } 
-//     } 
-//   } 
-// }) 
