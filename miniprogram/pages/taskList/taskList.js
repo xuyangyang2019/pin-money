4b53b637f7e4e9f870c042208035c056a96fe6f4
xuyangyang2019 <15268296911@163.com>
2022-01-26 16:18:52 +0800

    添加和删除数据
    并且更新本地数据

--- miniprogram/pages/taskList/taskList.js
+++ miniprogram/pages/taskList/taskList.js
@@ -8,6 +8,8 @@ Page({
   data: {
     openid: '',
     taskList: [],
+    taskName: '',
+    taskReward: ''
   },
 
   /**
@@ -66,52 +68,101 @@ Page({
     console.log('用户点击右上角分享')
   },
 
-  onAddTask: () => {
-    console.log('onAddTask')
-    
-    wx.showModal({
-      title: '提示',
-      content: '',
-      placeholderText: '请输入任务名称',
-      editable: true,
-      success(res) {
-        if (res.confirm) {
-          console.log('用户点击确定', res.content)
-          const db = wx.cloud.database()
-          db.collection('task').add({
-            data: {
-              name: res.content,
-              price: 1
-            },
-            success: res => {
-              console.log(res)
-              console.log(this)
-              // 在返回结果中会包含新创建的记录的 _id
-              this.setData({
-                taskList: taskList.push({
-                  name: res.content,
-                  price: 1
-                }),
-              })
-              // wx.showToast({
-              //   title: '新增记录成功',
-              // })
-              // console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
-              // this.onQueryTask()
-            },
-            fail: err => {
-              wx.showToast({
-                icon: 'none',
-                title: '新增记录失败'
-              })
-              console.error('[数据库] [新增记录] 失败：', err)
-            }
-          })
-        } else if (res.cancel) {
-          console.log('用户点击取消')
-        }
-      }
+  bindNameInput: function (e) {
+    this.setData({
+      taskName: e.detail.value
     })
+  },
+  bindRewardInput: function (e) {
+    this.setData({
+      taskReward: e.detail.value
+    })
+  },
+  onAddTask: function () {
+    if (this.data.taskReward && this.data.taskName) {
+      const db = wx.cloud.database()
+      db.collection('task').add({
+        data: {
+          name: this.data.taskName,
+          reward: this.data.taskReward
+        },
+        success: res => {
+          // 在返回结果中会包含新创建的记录的 _id
+          let oldData = [...this.data.taskList, {
+            _id: res._id,
+            _openid: app.globalData.openid,
+            name: this.data.taskName,
+            price: this.data.taskReward
+          }]
+          this.setData({
+            taskList: oldData,
+            taskName: '',
+            taskReward: ''
+          })
+          wx.showToast({
+            title: '新增记录成功',
+          })
+          // console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '新增记录失败'
+          })
+          console.error('[数据库] [新增记录] 失败：', err)
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '数据格式不正确，不能添加！',
+      })
+    }
+    // wx.showModal({
+    //   title: '提示',
+    //   content: '',
+    //   placeholderText: '请输入任务名称',
+    //   editable: true,
+    //   success(res) {
+    //     if (res.confirm) {
+    //       console.log('用户点击确定', res.content)
+    //       })
+    //     } else if (res.cancel) {
+    //       console.log('用户点击取消')
+    //     }
+    //   }
+  },
+  onRemove: function (e) {
+    // console.log('onRemove', e.currentTarget.dataset.task)
+    let taskId = null
+    if (e.currentTarget.dataset.task) {
+      taskId = e.currentTarget.dataset.task._id
+    }
+    if (taskId) {
+      const db = wx.cloud.database()
+      db.collection('task').doc(taskId).remove({
+        success: res => {
+          wx.showToast({
+            title: '删除成功',
+          })
+          this.setData({
+            taskList: this.data.taskList.filter(x => {
+              return x._id !== taskId
+            })
+          })
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '删除失败',
+          })
+          console.error('[数据库] [删除记录] 失败：', err)
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '无记录可删，请见创建一个记录',
+      })
+    }
   },
 
   onQueryTask: function () {
@@ -137,6 +188,7 @@ Page({
       }
     })
   },
+
 
   onCounterInc: function () {
     const db = wx.cloud.database()
@@ -176,33 +228,8 @@ Page({
     })
   },
 
-  onRemove: function () {
-    if (this.data.counterId) {
-      const db = wx.cloud.database()
-      db.collection('counters').doc(this.data.counterId).remove({
-        success: res => {
-          wx.showToast({
-            title: '删除成功',
-          })
-          this.setData({
-            counterId: '',
-            count: null,
-          })
-        },
-        fail: err => {
-          wx.showToast({
-            icon: 'none',
-            title: '删除失败',
-          })
-          console.error('[数据库] [删除记录] 失败：', err)
-        }
-      })
-    } else {
-      wx.showToast({
-        title: '无记录可删，请见创建一个记录',
-      })
-    }
-  },
+
+
   upper(e) {
     console.log(e)
   },
