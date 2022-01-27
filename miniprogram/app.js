a03d6b3c4d05479b3b265c5042c84ebe25ad0240
xuyangyang2019 <15268296911@163.com>
2022-01-27 15:09:20 +0800

    公共样式

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,16 +1,37 @@
 //app.js
 App({
   onLaunch: function () {
-    
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
         traceUser: true,
-        env:'test-146584'
+        env: 'test-146584'
       })
     }
-
     this.globalData = {}
-  }
+  },
+  onShow(options) {
+    // 获取openid
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.OPENID)
+        this.globalData.openid = res.result.OPENID
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+  onHide() {
+    // Do something when hide.
+  },
+  onError(msg) {
+    console.log(msg)
+  },
 })
