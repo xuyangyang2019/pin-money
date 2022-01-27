141d509cbf19b962c4323a8beb1cc935fb63420e
xuyangyang2019 <15268296911@163.com>
2022-01-27 17:32:21 +0800

    修改首页ui

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,6 +4,9 @@ const app = getApp()
 Page({
   data: {
     childrenList: [],
+    taskList: [],
+    userName: '',
+
     avatarUrl: './user-unlogin.png',
     userInfo: {},
     logged: false,
@@ -41,10 +44,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.onChildren()
+    this.onQueryChildren()
+    this.onQueryTask()
   },
 
-  onChildren: function () {
+  onQueryChildren: function () {
     const db = wx.cloud.database()
     // 查询当前用户所有的 counters
     db.collection('children').where({
@@ -54,7 +58,6 @@ Page({
         console.log('[数据库] [查询记录] 成功: ', res)
         this.setData({
           childrenList: res.data
-          // taskList: JSON.stringify(res.data, null, 2)
         })
       },
       fail: err => {
@@ -66,7 +69,79 @@ Page({
       }
     })
   },
-                
+  onQueryTask: function () {
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('task').where({
+      _openid: app.globalData.openid
+    }).get({
+      success: res => {
+        console.log('[数据库] [查询记录] 成功: ', res)
+        this.setData({
+          taskList: res.data
+        })
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        // console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+
+  /**
+   * 修改执行人名称
+   */
+  bindNameInput: function (e) {
+    this.setData({
+      userName: e.detail.value
+    })
+  },
+
+  /**
+   * 添加任务
+   */
+  onAddUser: function () {
+    if (this.data.userName) {
+      const db = wx.cloud.database()
+      db.collection('children').add({
+        data: {
+          name: this.data.userName,
+        },
+        success: res => {
+          // 在返回结果中会包含新创建的记录的 _id
+          let oldData = [...this.data.childrenList, {
+            _id: res._id,
+            _openid: app.globalData.openid,
+            name: this.data.userName,
+          }]
+          this.setData({
+            childrenList: oldData,
+            userName: '',
+          })
+          wx.showToast({
+            title: '新增记录成功',
+          })
+          // console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '新增记录失败'
+          })
+          console.error('[数据库] [新增记录] 失败：', err)
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '数据格式不正确，不能添加！',
+        icon: 'error'
+      })
+    }
+  },
+
   // 上传图片
   doUpload: function () {
     // 选择图片
